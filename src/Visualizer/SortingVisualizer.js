c717ce70f438c757f66b95b71e0fa3a48860a9f1
Pedro Machado <machadop1407@gmail.com>
2020-07-05 21:51:33 -0300

    added bubble sort, animations, and buttons

--- src/Visualizer/SortingVisualizer.js
+++ src/Visualizer/SortingVisualizer.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import * as BubbleSort from "../SortingAlgorithms/BubbleSort";
 
 const ARRAYSIZE = 100;
 
 export default function SortingVisualizer() {
   const [primaryArray, setPrimaryArray] = useState([]);
+  const currentBar = useRef();
 
   useEffect(() => {
     randomizeArray();
@@ -12,7 +14,7 @@ export default function SortingVisualizer() {
   function randomizeArray() {
     var array = [];
     for (var i = 0; i < ARRAYSIZE; i++) {
-      array.push(randomVals(20, 200));
+      array.push(randomVals(20, 400));
     }
 
     setPrimaryArray(array);
@@ -23,19 +25,67 @@ export default function SortingVisualizer() {
     return randomVal;
   }
 
+  //SLEEP FUNCTION --> Used to slow loop iteration
+  const sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
+
+  // ------------ ALGORITHMS ------------ //
+
+  // Bubble Sort
+  async function bubbleSort() {
+    var currentArr = primaryArray;
+
+    var sorted = false;
+
+    while (!sorted) {
+      sorted = true;
+
+      for (var i = 0; i < currentArr.length - 1; i++) {
+        if (currentArr[i] > currentArr[i + 1]) {
+          var swap1 = currentArr[i];
+          var swap2 = currentArr[i + 1];
+          currentArr[i] = swap2;
+          currentArr[i + 1] = swap1;
+          setPrimaryArray([...primaryArray, currentArr]);
+
+          //Changes the Style
+          let bar1 = document.getElementById(i).style;
+          let bar2 = document.getElementById(i + 1).style;
+          bar1.backgroundColor = "crimson";
+          bar2.backgroundColor = "slateblue";
+
+          await sleep(50);
+
+          bar1.backgroundColor = "coral";
+          bar2.backgroundColor = "coral";
+
+          sorted = false;
+        }
+      }
+    }
+  }
+
   return (
     <div className="sortingVisualizer">
       <div className="header">
         <div className="headerBttns">
-          <button>Generate New Array</button>
-          <button>Generate New Array</button>
-          <button>Generate New Array</button>
+          <button onClick={randomizeArray}>New Array</button>
+          <button onClick={bubbleSort}>Bubble Sort</button>
+          <button>...</button>
         </div>
       </div>
       <div className="arrayContainer">
         {primaryArray &&
           primaryArray.map((val, key) => {
-            return <div id="bar" key={key} style={{ height: val }}></div>;
+            return (
+              <div
+                className="bar"
+                id={key}
+                key={key}
+                style={{ height: val }}
+              ></div>
+            );
           })}
       </div>
     </div>
