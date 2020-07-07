4aaae3ac2cafeae613684d86e3fc2fe9628ad66f
Pedro Machado <machadop1407@gmail.com>
2020-07-07 08:29:42 -0300

    added heap sort

--- src/Visualizer/SortingVisualizer.js
+++ src/Visualizer/SortingVisualizer.js
@@ -10,6 +10,9 @@ export default function SortingVisualizer() {
   //Animation Speed
   const [animationSpeed, setAnimationSpeed] = useState(40);
 
+  //AlgorithmChosen
+  const [algorithm, setAlgorithm] = useState({});
+
   //Initial Random Array
   useEffect(() => {
     randomizeArray();
@@ -46,6 +49,8 @@ export default function SortingVisualizer() {
 
     var sorted = false;
 
+    setAlgorithm({ name: "Bubble Sort", timeComplexity: "O(n^2)" });
+
     while (!sorted) {
       sorted = true;
 
@@ -75,13 +80,108 @@ export default function SortingVisualizer() {
     }
   }
 
+  //HeapSort
+  async function heapSort() {
+    var arr = primaryArray;
+
+    var length = arr.length;
+    var index = Math.floor(length / 2 - 1); //This is always the last parent in a heap
+    var lastChild = length - 1;
+
+    while (index >= 0) {
+      heapify(arr, length, index);
+      index--;
+
+      setPrimaryArray([...primaryArray, arr]);
+
+      //Changes the Style while swapping
+      if (index >= 0) {
+        console.log(document.getElementById(index));
+        let bar1 = document.getElementById(index).style;
+        let bar2 = document.getElementById(index + 1).style;
+        bar1.backgroundColor = "#DC143C";
+        bar2.backgroundColor = "#6A5ACD";
+
+        await sleep(animationSpeed);
+
+        //Changes the Style back to original
+        bar1.backgroundColor = "#FF7F50";
+        bar2.backgroundColor = "#FF7F50";
+      } else {
+        await sleep(animationSpeed);
+      }
+    }
+
+    while (lastChild >= 0) {
+      var swap1 = arr[0];
+      var swap2 = arr[lastChild];
+
+      arr[0] = swap2;
+      arr[lastChild] = swap1;
+      heapify(arr, lastChild, 0);
+      lastChild--;
+
+      setPrimaryArray([...primaryArray, arr]);
+
+      //Changes the Style while swapping
+      if (index >= 0) {
+        let bar1 = document.getElementById(lastChild).style;
+        let bar2 = document.getElementById(0).style;
+        bar1.backgroundColor = "#DC143C";
+        bar2.backgroundColor = "#6A5ACD";
+
+        await sleep(animationSpeed);
+
+        //Changes the Style back to original
+        bar1.backgroundColor = "#FF7F50";
+        bar2.backgroundColor = "#FF7F50";
+      } else {
+        await sleep(animationSpeed);
+      }
+    }
+  }
+
+  async function heapify(arr, length, index) {
+    //Represents larges node out of the three
+    var largest = index;
+
+    //Left Child
+    var leftNode = index * 2 + 1;
+    //Right Child
+    var rightNode = leftNode + 1;
+
+    //Check if left is largest, check if reached end
+    if (arr[leftNode] > arr[largest] && leftNode < length) {
+      largest = leftNode;
+    }
+
+    //Check if right is largest, check if reached end
+    if (arr[rightNode] > arr[largest] && rightNode < length) {
+      largest = rightNode;
+    }
+
+    //Check if parent is still largest, if not: perform a swap between the smallest and the largest
+    if (largest != index) {
+      var swap1 = arr[index];
+      var swap2 = arr[largest];
+
+      arr[index] = swap2;
+      arr[largest] = swap1;
+
+      heapify(arr, length, largest);
+    }
+
+    return arr;
+  }
+
   return (
     <div className="sortingVisualizer">
       <div className="header">
+        {/* Algorithm Buttons */}
         <div className="headerBttns">
           <button onClick={randomizeArray}>New Array</button>
           <button onClick={bubbleSort}>Bubble Sort</button>
-          <button onClick={bubbleSort}>Heap Sort</button>
+          <button onClick={heapSort}>Heap Sort</button>
           <button onClick={bubbleSort}>Quick Sort</button>
           <button onClick={bubbleSort}>Merge Sort</button>
         </div>
@@ -136,6 +236,17 @@ export default function SortingVisualizer() {
               ></div>
             );
           })}
+      </div>
+
+      <div className="algorithmInfo">
+        {algorithm.name != undefined && (
+          <>
+            <div id="name">Algorithm: {algorithm.name}</div>
+            <div id="timeComplexity">
+              Time Complexity: {algorithm.timeComplexity}{" "}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
